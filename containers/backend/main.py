103706ab170867bef54f21a54ba13c842680877d
kettleTeacues <teacues@gmail.com>
2024-03-30 17:07:51 +0000

    CORS制御を追加

--- containers/backend/main.py
+++ containers/backend/main.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI
 
 from routers import hello
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(hello.router)
@@ -10,6 +11,14 @@ app.include_router(hello.router)
 origins = [
     "http://localhost:3000",
 ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
